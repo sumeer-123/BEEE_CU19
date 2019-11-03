b2b853700349ece9918e9752e157913c01a66c56
sumeer-123 <55073746+sumeer-123@users.noreply.github.com>
2019-11-03 19:08:05 +0530

    Create code.ino

---
+++ experiment 6/code.ino
@@ -0,0 +1,23 @@
+const int trigPin = 9;
+const int echoPin = 10;
+
+long duration;
+int distance;
+void setup()
+{
+pinMode(trigPin,OUTPUT);
+pinMode(echoPin,INPUT);
+Serial.begin(9600);
+}
+void loop()
+{
+digital.Write(trigPin,LOW);
+delayMicroseconds(2);
+digitalWrite(trigPin,HIGH);
+delayMicroseconds(10);
+digital.Write(trigpin ,LOW);
+
+distance=duration* 0.017;
+Serial.print("Distance : ");
+Serial.println("distance );
+}
